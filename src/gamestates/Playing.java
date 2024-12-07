437789558f78ef272d21a21e3f942cf1490b15cd
Snuc1925 <maimanh1905@gmail.com>
2024-12-07 23:38:29 +0700

    fix energyOrb bug

--- src/gamestates/Playing.java
+++ src/gamestates/Playing.java
@@ -56,7 +56,7 @@ public class Playing extends State implements Statemethods {
     public Npc[] npcArray;
 
     // Level
-    public String currentLevel = "level1";
+    public String currentLevel = "level4";
     public EnergyOrb energyOrb = null;
     public NextLevel nextLevel = null;
 
@@ -155,10 +155,16 @@ public class Playing extends State implements Statemethods {
             Gamestate.state = Gamestate.GAME_OVER;
         }
 
-        if  (currentLevel.equals("level4") && monsters.size() == 1) {
-            Monster monster = monsters.get(0);
-            if (monster.image == null && monster.currentState == EntityState.DEATH) {
-                energyOrb = new EnergyOrb(this, 1150, 410);
+        if  (currentLevel.equals("level4") && energyOrb == null) {
+            boolean allMonstersDeath = true;
+            for (Monster monster : monsters) {
+                if (monster.currentState != EntityState.DEATH || monster.image != null) {
+                    allMonstersDeath = false;
+                    break;
+                }
+            }
+            if (allMonstersDeath) {
+                energyOrb = new EnergyOrb(this, 1220, 577);
             }
         }
 
